92983c946f74b927913b692e707b36d24eb3e001
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2023-03-27 16:56:18 +0200

    Update .gitignore

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
     match request.method:
         case 'GET':
             template = 'blog/index.html'
-            context = {}
+            context = {
+                'categories': models.Category.objects.filter(is_enabled=True),
+                'articles': models.Article.objects.filter(is_enabled=True),
+            }
 
             return render(request, template, context)
 
