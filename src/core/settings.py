018bd3a38c866334d00600ef2dbe9a4358cf2276
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2023-03-27 15:50:00 +0200

    Use postgres instead of sqlite3

--- src/core/settings.py
+++ src/core/settings.py
@@ -55,8 +55,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-blog',
+        'USER': 'admin',
+        'PASSWORD': 'password123',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
