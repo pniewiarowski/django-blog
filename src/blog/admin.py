f914f7e63ddf6ea0f3e53bc9a29f16badf2d0a8d
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2023-03-27 16:37:22 +0200

    Add docblock

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+"""
+Register models from blog application, to
+django admin page.
+"""
 for model in [
     models.Category,
     models.Article,
